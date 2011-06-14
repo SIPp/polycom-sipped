6f1de9f680faa43fd68419d98228657a94525934
edward_estabrook <edward_estabrook@85c8ee13-dc6b-4ec3-b49e-93a6737f2991>
2011-06-14 21:08:29 +0000

    BugID(s): VOIP-64473
    v3.2.14 BETA2-TLS-PCAP checked in.
    
    
    git-svn-id: https://subversion.polycom.com/SVN/RepoMisc/TestUtilities/trunk/SIPped/SIPped/src@24000 85c8ee13-dc6b-4ec3-b49e-93a6737f2991

--- sipp.cpp
+++ sipp.cpp
@@ -4302,7 +4302,7 @@ int main(int argc, char *argv[])
 	  }
 	  exit(EXIT_OTHER);
 	case SIPP_OPTION_VERSION:
-	  printf("\n SIPped v3.2.14BETA"
+	  printf("\n SIPped v3.2.14BETA2"
 #ifdef _USE_OPENSSL
 	      "-TLS"
 #endif
