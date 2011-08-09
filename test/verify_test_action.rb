324d4e0a194cf52c2b3b88d85987f23f8d2bc082
daniel_busto <daniel_busto@85c8ee13-dc6b-4ec3-b49e-93a6737f2991>
2011-08-09 23:09:28 +0000

    BugID(s): VOIP-64473
    Fix Description:
    Updated expected error message to reflect corrected and improved message when test fails.
    
    
    git-svn-id: https://subversion.polycom.com/SVN/RepoMisc/TestUtilities/trunk/SIPped/SIPped/src@25166 85c8ee13-dc6b-4ec3-b49e-93a6737f2991

--- test/verify_test_action.rb
+++ test/verify_test_action.rb
@@ -17,7 +17,7 @@ class VerifyTestAction < Test::Unit::TestCase
   end
   def test_test_fail
     negative_test = SippTest.new("verify_test_action_fail", "-sf verify_test_action_fail_client.sipp -mc", "-sf verify_test_action_fail_server.sipp -mc")
-    negative_test.expected_error_log = /Test firstcseq ==  2 INVITE has failed/
+    negative_test.expected_error_log = /Test firstcseq == secondcseq has failed because 1.000000 == 2.000000 is NOT true/
     assert(negative_test.run())
   end
 end
