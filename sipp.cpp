3a178151975345c4bd080835df7fcdf7f23e2001
edward_estabrook <edward_estabrook@85c8ee13-dc6b-4ec3-b49e-93a6737f2991>
2011-08-09 23:15:15 +0000

    BugID(s):  VOIP-64473
    Fix Description:
    Update version number to .15
    
    
    git-svn-id: https://subversion.polycom.com/SVN/RepoMisc/TestUtilities/trunk/SIPped/SIPped/src@25170 85c8ee13-dc6b-4ec3-b49e-93a6737f2991

--- sipp.cpp
+++ sipp.cpp
@@ -4306,7 +4306,7 @@ int main(int argc, char *argv[])
 	  }
 	  exit(EXIT_OTHER);
 	case SIPP_OPTION_VERSION:
-	  printf("\n SIPped v3.2.15Beta1"
+	  printf("\n SIPped v3.2.15"
 #ifdef _USE_OPENSSL
 	      "-TLS"
 #endif
