e70233019c95d4add66f762706d1251bad9e48af
edward_estabrook <edward_estabrook@85c8ee13-dc6b-4ec3-b49e-93a6737f2991>
2011-10-24 23:42:37 +0000

    BugID(s):  VOIP-64473
    Changelog, update version to non-BETA status.
    
    
    git-svn-id: https://subversion.polycom.com/SVN/RepoMisc/TestUtilities/trunk/SIPped/SIPped/src@26660 85c8ee13-dc6b-4ec3-b49e-93a6737f2991

--- sipp.cpp
+++ sipp.cpp
@@ -4302,7 +4302,7 @@ int main(int argc, char *argv[])
 	  }
 	  exit(EXIT_OTHER);
 	case SIPP_OPTION_VERSION:
-	  printf("\n SIPped v3.2.18-BETA"
+	  printf("\n SIPped v3.2.18"
 #ifdef _USE_OPENSSL
 	      "-TLS"
 #endif
