47d9eef1d3820a566951a4ca107721edb249c930
daniel_busto <daniel_busto@85c8ee13-dc6b-4ec3-b49e-93a6737f2991>
2011-12-09 18:23:07 +0000

    BugID(s): VOIP-64473
    Problem Description:
    Typo in exec.rb file corrected.
    
    
    git-svn-id: https://subversion.polycom.com/SVN/RepoMisc/TestUtilities/trunk/SIPped/SIPped/src@27543 85c8ee13-dc6b-4ec3-b49e-93a6737f2991

--- test/exec.rb
+++ test/exec.rb
@@ -57,7 +57,7 @@ class Exec < Test::Unit::TestCase
 
   def test_exec_uses_correct_path
 	# verify <exec verify="rsipp.pl ... ">
-    test = SippTest.new("exec_uses_correct_path", "-mc -sf exec_uses_correct_path", "-sn uas -aa ")
+    test = SippTest.new("exec_uses_correct_path", "-mc -sf exec_uses_correct_path.sipp", "-sn uas -aa ")
 	test.expected_minimum_run_time = 3
     assert(test.run())
   end
