3131357b597dcbedc605bb21ab82c9f4483e97fd
Edward.Estabrook@Polycom.com <Edward.Estabrook@Polycom.com@85c8ee13-dc6b-4ec3-b49e-93a6737f2991>
2012-01-26 21:42:43 +0000

    BugID(s): VOIP-64473
    Bump version number to 3.2.21
    
    git-svn-id: https://subversion.polycom.com/SVN/RepoMisc/TestUtilities/trunk/SIPped/SIPped/src@28405 85c8ee13-dc6b-4ec3-b49e-93a6737f2991

--- sipp.cpp
+++ sipp.cpp
@@ -4414,7 +4414,7 @@ int main(int argc, char *argv[])
 	  }
 	  exit(EXIT_OTHER);
 	case SIPP_OPTION_VERSION:
-	  printf("\n SIPped v3.2.21-BETA"
+	  printf("\n SIPped v3.2.21"
 #ifdef _USE_OPENSSL
 	      "-TLS"
 #endif
