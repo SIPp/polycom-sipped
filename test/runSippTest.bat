e8d3f1a750c1257928ff38a0dfa91de9e2272e75
Richard.Lum@Polycom.com <Richard.Lum@Polycom.com@85c8ee13-dc6b-4ec3-b49e-93a6737f2991>
2012-04-02 20:33:50 +0000

    BugID(s): TPER-5113
    runSippTest.bat configured to set up env variables assuming jenkins invokes
    
    git-svn-id: https://subversion.polycom.com/SVN/RepoMisc/TestUtilities/trunk/SIPped/SIPped/src@30326 85c8ee13-dc6b-4ec3-b49e-93a6737f2991

--- test/runSippTest.bat
+++ test/runSippTest.bat
@@ -1,8 +1,16 @@
 REM 
 REM	invoke ruby sipp test and copy xml results to unittest results directory
-REM
+REM assume invoke from windows running jenkins
 REM
 
+set TA_DIR=%WORKSPACE%\TestUtilities
+set SIPPED=%WORKSPACE%\TestUtilities\SIPped\WindowsBinary\SIPped
+set CYGWIN=nodosfilewarning
+
+copy /Y  C:\TA_Config\SIPped\rsipp\rsipp.config.xml "%TA_DIR%\SIPped\rsipp\"
+cat "%TA_DIR%\SIPped\rsipp\rsipp.config.xml"
+
+cd "%TA_DIR%\SIPped\SIPped\src\test"
 rake
-echo "returned from rake"
+echo "$?   returned from rake"
 xcopy /Y testresults\*.* ..\UnitTest\testresults
