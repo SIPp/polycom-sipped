54ca6761faedbf350affd1749039c798e8c2cb71
Edward.Estabrook@Polycom.com <Edward.Estabrook@Polycom.com@85c8ee13-dc6b-4ec3-b49e-93a6737f2991>
2012-05-02 22:50:06 +0000

    BugID(s): VOIP-64473
    Add test coverage for loose_message_routing.
    
    git-svn-id: https://subversion.polycom.com/SVN/RepoMisc/TestUtilities/trunk/SIPped/SIPped/src@31069 85c8ee13-dc6b-4ec3-b49e-93a6737f2991

--- test/loose_message_sequencing.rb
+++ test/loose_message_sequencing.rb
@@ -11,29 +11,43 @@ require './sipp_test'
 
 class LooseMessageSequencing < Test::Unit::TestCase
   def test_bla_register_and_subscribe_two_lines_short_in_sequence
+    # messages delivered in order, no optional messages.
     test = SippTest.new("test_bla_register_and_subscribe_two_lines_short_in_sequence", "-sf bla_register_and_subscribe_two_lines_short_client.sipp -mc", "-sf bla_register_and_subscribe_two_lines_short_in_sequence_server.sipp -mc")
     assert(test.run())
   end
   
-  def test_bla_register_and_subscribe_two_lines_short_with_optional_client
-    test = SippTest.new("test_bla_register_and_subscribe_two_lines_short_with_optional_client", "-sf bla_register_and_subscribe_two_lines_short_with_optional_client.sipp -mc", "-sf bla_register_and_subscribe_two_lines_short_in_sequence_server.sipp -mc")
+  def test_bla_register_and_subscribe_two_lines_short_with_optional_client_messages_that_are_never_sent
+    test = SippTest.new("test_bla_register_and_subscribe_two_lines_short_with_optional_client_messages_that_are_never_sent", "-sf bla_register_and_subscribe_two_lines_short_with_optional_client.sipp -mc", "-sf bla_register_and_subscribe_two_lines_short_in_sequence_server.sipp -mc")
     assert(test.run())
   end  
 
-  # def test_bla_register_and_subscribe_two_lines_short_register_and_200_swapped_server_will_fail
-    # test = SippTest.new("bla_register_and_subscribe_two_lines_short_in_sequence", "-sf bla_register_and_subscribe_two_lines_short_with_optional_client.sipp -mc", "-sf bla_register_and_subscribe_two_lines_short_register_and_200_swapped_server.sipp -mc")
-    # test.expected_exitstatus = 123
-    # test.expected_errormessage = something
-    # assert(test.run())
-  # end    
+
+  def test_bla_register_and_subscribe_two_lines_short_with_optional_client_messages_that_are_sent_several_times
+    test = SippTest.new("test_bla_register_and_subscribe_two_lines_short_with_optional_client_messages_that_are_sent_several_times", "-sf bla_register_and_subscribe_two_lines_short_with_optional_client.sipp -mc", "-sf bla_register_and_subscribe_two_lines_short_in_sequence_server_optional_messages_sent_server.sipp -mc")
+    assert(test.run())
+  end  
+
 
   def test_bla_register_and_subscribe_two_lines_short_register_and_200_swapped_server_will_succeed
-    # will need to add parameter to enable (perhaps its on by default with -mc).
     test = SippTest.new("test_bla_register_and_subscribe_two_lines_short_register_and_200_swapped_server_will_succeed", "-sf bla_register_and_subscribe_two_lines_short_with_optional_client.sipp -mc", "-sf bla_register_and_subscribe_two_lines_short_register_and_200_swapped_server.sipp -mc")
     assert(test.run())
   end    
   
+ 
+  def test_bla_register_and_subscribe_two_lines_short_register_and_200_swapped_with_optional_server_will_succeed
+    test = SippTest.new("test_bla_register_and_subscribe_two_lines_short_register_and_200_swapped_with_optional_server_will_succeed", "-sf bla_register_and_subscribe_two_lines_short_register_and_200_swapped_with_optional_server.sipp -mc", "-sf bla_register_and_subscribe_two_lines_short_register_and_200_swapped_server.sipp -mc")
+    assert(test.run())
+  end    
 
+
+  def test_bla_register_and_subscribe_two_lines_short_register_and_200_swapped_with_optional_too_early_will_fail
+    test = SippTest.new("bla_register_and_subscribe_two_lines_short_in_sequence", "-sf bla_register_and_subscribe_two_lines_short_with_optional_client.sipp -mc", "-sf bla_register_and_subscribe_two_lines_short_register_and_200_swapped_with_optional_too_early_server.sipp -mc")
+    test.expected_exitstatus = 123
+    test.expected_errormessage = "Unexpected message RANDOME received while expecting REGISTER on dialog 4"
+    assert(test.run())
+  end    
+  
+# need more negative tests here...
   
 end
 
