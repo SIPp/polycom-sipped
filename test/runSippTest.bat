4da518081d4476d93f0ec669b4c1bc45bd94b1ba
Richard.Lum@Polycom.com <Richard.Lum@Polycom.com@85c8ee13-dc6b-4ec3-b49e-93a6737f2991>
2012-06-13 21:10:12 +0000

    BugID(s): VOIP-64473
    runnSippTest.bat updated to reflect new location of c:\TA_config\CI-TA-01\SIPped\rsipp\rsipp.config.xml  for jenkins
    
    git-svn-id: https://subversion.polycom.com/SVN/RepoMisc/TestUtilities/trunk/SIPped/SIPped/src@32199 85c8ee13-dc6b-4ec3-b49e-93a6737f2991

--- test/runSippTest.bat
+++ test/runSippTest.bat
@@ -7,7 +7,9 @@ set TA_DIR=%WORKSPACE%\TestUtilities
 set SIPPED=%WORKSPACE%\TestUtilities\SIPped\WindowsBinary\SIPped
 set CYGWIN=nodosfilewarning
 
-copy /Y  C:\TA_Config\SIPped\rsipp\rsipp.config.xml "%TA_DIR%\SIPped\rsipp\"
+rem copy /Y  C:\TA_Config\SIPped\rsipp\rsipp.config.xml "%TA_DIR%\SIPped\rsipp\"
+copy /Y  c:\TA_config\CI-TA-01\SIPped\rsipp\rsipp.config.xml "%TA_DIR%\SIPped\rsipp\"
+
 cat "%TA_DIR%\SIPped\rsipp\rsipp.config.xml"
 
 cd "%TA_DIR%\SIPped\SIPped\src\test"
