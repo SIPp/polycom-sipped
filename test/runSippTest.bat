aa6e65bed10904871929813745e08a5276617f3d
Richard.Lum@Polycom.com <Richard.Lum@Polycom.com@85c8ee13-dc6b-4ec3-b49e-93a6737f2991>
2012-06-13 22:03:58 +0000

    BugID(s): VOIP-64473
    runnSippTest.bat updated to reflect checkout of rsipp.config.xml from  new source location : \TestUtilities\AutoTester\continuous_integration\TA_Config\CI-TA-01\ into %TA_DIR%\tmp.  Batch file now copies from tmp to Sipped/rsipp
    
    git-svn-id: https://subversion.polycom.com/SVN/RepoMisc/TestUtilities/trunk/SIPped/SIPped/src@32202 85c8ee13-dc6b-4ec3-b49e-93a6737f2991

--- test/runSippTest.bat
+++ test/runSippTest.bat
@@ -8,7 +8,7 @@ set SIPPED=%WORKSPACE%\TestUtilities\SIPped\WindowsBinary\SIPped
 set CYGWIN=nodosfilewarning
 
 rem copy /Y  C:\TA_Config\SIPped\rsipp\rsipp.config.xml "%TA_DIR%\SIPped\rsipp\"
-copy /Y  c:\TA_config\CI-TA-01\SIPped\rsipp\rsipp.config.xml "%TA_DIR%\SIPped\rsipp\"
+copy /Y  %TA_DIR%\tmp\rsipp.config.xml "%TA_DIR%\SIPped\rsipp\"
 
 cat "%TA_DIR%\SIPped\rsipp\rsipp.config.xml"
 
