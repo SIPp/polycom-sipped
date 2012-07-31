d3e6c75a2c3c432dbfd30f6af04ed6fa0868aa97
Richard.Lum@Polycom.com <Richard.Lum@Polycom.com@85c8ee13-dc6b-4ec3-b49e-93a6737f2991>
2012-07-31 20:10:20 +0000

    BugID(s): VOIP-64473
    fix win32make.bat file for compiling win 32 SIPped 
    
    git-svn-id: https://subversion.polycom.com/SVN/RepoMisc/TestUtilities/trunk/SIPped/SIPped/src@32921 85c8ee13-dc6b-4ec3-b49e-93a6737f2991

--- win32make.bat
+++ win32make.bat
@@ -3,7 +3,41 @@ rem *   20120731  compile windows port of sipped
 rem ******************************************************************
 
 rem Set the environment for msbuild
-"C:\Program Files (x86)\Microsoft Visual Studio 8\VC\vcvarsall.bat"
+rem "C:\Program Files (x86)\Microsoft Visual Studio 8\VC\vcvarsall.bat"
+@SET VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 8
+@SET VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 8\VC
+@SET FrameworkDir=C:\Windows\Microsoft.NET\Framework
+@SET FrameworkVersion=v2.0.50727
+@SET FrameworkSDKDir=C:\Program Files (x86)\Microsoft Visual Studio 8\SDK\v2.0
+@if "%VSINSTALLDIR%"=="" goto error_no_VSINSTALLDIR
+@if "%VCINSTALLDIR%"=="" goto error_no_VCINSTALLDIR
+
+@echo Setting environment for using Microsoft Visual Studio 2005 x86 tools.
+
+@rem
+@rem Root of Visual Studio IDE installed files.
+@rem
+@set DevEnvDir=C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE
+
+@set PATH=C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE;C:\Program Files (x86)\Microsoft Visual Studio 8\VC\BIN;C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\Tools;C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\Tools\bin;C:\Program Files (x86)\Microsoft Visual Studio 8\VC\PlatformSDK\bin;C:\Program Files (x86)\Microsoft Visual Studio 8\SDK\v2.0\bin;C:\Windows\Microsoft.NET\Framework\v2.0.50727;C:\Program Files (x86)\Microsoft Visual Studio 8\VC\VCPackages;%PATH%
+@set INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio 8\VC\ATLMFC\INCLUDE;C:\Program Files (x86)\Microsoft Visual Studio 8\VC\INCLUDE;C:\Program Files (x86)\Microsoft Visual Studio 8\VC\PlatformSDK\include;C:\Program Files (x86)\Microsoft Visual Studio 8\SDK\v2.0\include;%INCLUDE%
+@set LIB=C:\Program Files (x86)\Microsoft Visual Studio 8\VC\ATLMFC\LIB;C:\Program Files (x86)\Microsoft Visual Studio 8\VC\LIB;C:\Program Files (x86)\Microsoft Visual Studio 8\VC\PlatformSDK\lib;C:\Program Files (x86)\Microsoft Visual Studio 8\SDK\v2.0\lib;%LIB%
+@set LIBPATH=C:\Windows\Microsoft.NET\Framework\v2.0.50727;C:\Program Files (x86)\Microsoft Visual Studio 8\VC\ATLMFC\LIB
+
+@goto end
+
+:error_no_VSINSTALLDIR
+@echo ERROR: VSINSTALLDIR variable is not set. 
+@goto end
+
+:error_no_VCINSTALLDIR
+@echo ERROR: VCINSTALLDIR variable is not set. 
+@goto end
+
+:end
+
+
+
 msbuild SIPped.sln /t:rebuild
 
 rem put executable into src directory
