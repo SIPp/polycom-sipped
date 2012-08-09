72a5b11377707f81d6eaacf712f938e746c21260
Richard.Lum@Polycom.com <Richard.Lum@Polycom.com@85c8ee13-dc6b-4ec3-b49e-93a6737f2991>
2012-08-09 18:32:39 +0000

    BugID(s): VOIP-64473
    jenkins build env PATH not recognizing svn, explicit path to svn.exe
    
    git-svn-id: https://subversion.polycom.com/SVN/RepoMisc/TestUtilities/trunk/SIPped/SIPped/src@32971 85c8ee13-dc6b-4ec3-b49e-93a6737f2991

--- DeploySIPped.bat
+++ DeploySIPped.bat
@@ -15,6 +15,6 @@ dir
 rem 
 rem svn commit %1 "%SIPPED%\sipp.exe" --username jenkins  --password jenkins  --non-interactive -m "BugID(s): VOIP-64473\n\n%1 commit"
 rem "c:\Program Files\SlikSvn\bin\svn.exe" commit %1 "%SIPPED%\sipp.exe" --username jenkins  --password jenkins  --non-interactive -F "%TA_DIR%\SIPped\SIPped\src\svncommitmessage.txt"
-svn commit %1 --non-interactive -F "%TA_DIR%\SIPped\SIPped\src\svncommitmessage.txt" --force-log
+"c:\Program Files\SlikSvn\bin\svn.exe" commit %1 --non-interactive -F "%TA_DIR%\SIPped\SIPped\src\svncommitmessage.txt" --force-log
 rem
   
