0e017e05d64ea18c71aa0ad0b6da65c02f8e7905
Edward Estabrook <Edward.Estabrook@Polycom.com>
2013-05-07 17:03:12 -0700

    Use bash not sh in makefile so pushd & popd work on mint Add Test::Critic package to package installer

--- rsipp/installPackages.sh
+++ rsipp/installPackages.sh
@@ -5,4 +5,4 @@ perl -MCPAN -e 'install Data::Dumper'
 perl -MCPAN -e 'install Socket'
 perl -MCPAN -e 'install Net::IP'
 perl -MCPAN -e 'install File::Slurp'
-
+perl -MCPAN -e 'install Test::Perl::Critic'
